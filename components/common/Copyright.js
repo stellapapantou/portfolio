25011c7e512ccac2b2e31c8323628ac5babcf0b2
stellapapantou <spapantou@gmail.com>
2025-01-05 13:50:14 +0200

    Update Copyright.js

--- components/common/Copyright.js
+++ components/common/Copyright.js
@@ -2,10 +2,10 @@ const Copyright = () => {
   return (
     <div className="container mx-auto grid lg:grid-cols-12 grid-cols-1 xl:gap-8 relative flex flex-col flex-auto">
         <div className="lg:col-span-6">
-            <p className="text-body-xs tracking-wide font-semibold uppercase text-neutral-600 dark:text-white">Thanks for stopping by!</p>
+            <p className="text-body-xs tracking-wide font-semibold uppercase text-center lg:text-left text-neutral-600 dark:text-white">Thanks for stopping by!</p>
         </div>
         <div className="lg:col-span-6">
-            <p className="text-body-xs tracking-wide font-semibold uppercase text-right text-neutral-600 dark:text-white">© Stella Papantou {(new Date().getFullYear())} : All Rights Reserved</p>
+            <p className="text-body-xs tracking-wide font-semibold uppercase text-center lg:text-right text-neutral-600 dark:text-white">© Stella Papantou {(new Date().getFullYear())} : All Rights Reserved</p>
         </div>
     </div>
   );
