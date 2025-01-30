3b2d83a658dd57625d742b9964842627ef7cfcf2
Stella Papantou <stella@192.168.1.5>
2025-01-30 13:11:25 +0200

    vercel analytics

--- components/common/Copyright.js
+++ components/common/Copyright.js
@@ -1,8 +1,12 @@
+import Link from "next/link";
+
 const Copyright = () => {
   return (
     <div className="container mx-auto grid lg:grid-cols-12 grid-cols-1 xl:gap-8 relative flex flex-col flex-auto">
         <div className="lg:col-span-6">
             <p className="text-body-xs tracking-wide font-semibold uppercase text-center lg:text-left text-neutral-600 dark:text-white">Thanks for stopping by!</p>
+            <p className="text-body-xs text-center lg:text-left text-neutral-600 dark:text-white my-1">This site uses <Link href="https://vercel.com/docs/analytics/privacy-policy" target="_blank" className="transition-all duration-300 no-underline hover:underline
+">Vercel Analytics</Link></p>
         </div>
         <div className="lg:col-span-6">
             <p className="text-body-xs tracking-wide font-semibold uppercase text-center lg:text-right text-neutral-600 dark:text-white">© Stella Papantou {(new Date().getFullYear())} : All Rights Reserved</p>
